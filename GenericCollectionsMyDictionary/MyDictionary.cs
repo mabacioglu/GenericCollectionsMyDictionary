734fb4adacc683719a3974488f5b484ff406c601
Mehmet ABACIOĞLU <mabacioglu@gmail.com>
2021-01-23 15:50:33 +0300

    Add project files.

---
+++ GenericCollectionsMyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsMyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+            keys = new K[tempKeys.Length+1];
+            values = new V[tempValues.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+                
+            }
+            keys[tempKeys.Length] = key;
+            values[tempValues.Length] = value;
+
+        }
+
+        public void PrintAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key : {0}",keys[i] );
+                Console.WriteLine("Value : {0}", values[i]);
+                Console.WriteLine("--------------------------");
+            }
+
+        }
+    }
+}
---
+++ GenericCollectionsMyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace GenericCollectionsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,int> myDictionary = new MyDictionary<int,int>();
+            myDictionary.Add(1, 1);
+            myDictionary.Add(1, 2);
+            myDictionary.PrintAll();
+
+            MyDictionary<string,bool> myDictionary1 = new MyDictionary<string, bool>();
+            myDictionary1.Add("Ankara", true);
+            myDictionary1.Add("İstanbul", false);
+            myDictionary1.PrintAll();
+
+        }
+    }
+}
